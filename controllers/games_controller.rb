bc233e4789a58ce396941d3aa5c203df71a30b97
mark91 <mark.mekhayl@gmail.com>
2022-12-13 20:40:50 +1100

    wishlist added

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -9,3 +9,31 @@ end
 get '/games/new' do
     erb :'games/new'
 end
+
+
+
+post '/games' do
+    name = params['name']
+    year_released = params['year_released']
+    
+    add_game(name, year_released)
+
+    redirect '/wishlist'
+
+end
+
+get '/wishlist' do
+
+    get_last_game = get_last_game()
+    erb :'/users/wishlist', locals: {
+        get_last_game: get_last_game
+    }
+end
+
+post '/games/:id/wishlist' do
+    game_id = params['id']
+    user_id = session['user_id']
+
+    run_sql('INSERT INTO wishlist(user_id, game_id) VALUES($1, $2)', [user_id, game_id])
+    redirect '/'
+  end
--- models/games.rb
+++ models/games.rb
@@ -1,3 +1,12 @@
 def games
     run_sql('SELECT * FROM games ORDER by id')
 end
+
+def add_game(name, year_released)
+    run_sql('INSERT INTO games(name, year_released) VALUES($1, $2)', [name, year_released])
+end
+
+def get_last_game
+    run_sql('SELECT * FROM games ORDER BY id DESC LIMIT 1')
+end
+    
